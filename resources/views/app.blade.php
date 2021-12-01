6b0c613e796fdcdc7b015034eec7c80986ad1b06
Ruben Steen <rubensteen123@gmail.com>
2021-12-01 14:14:16 +0000

    Added Vue/Inertia/TailwindUI/CSS

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
